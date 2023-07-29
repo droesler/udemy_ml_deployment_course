3c765fd4b72a9209d7169f7dfdc6573074eb5f8a
David Roesler <roeslerdavidthomas@gmail.com>
2023-07-29 16:38:50 -0700

    Started initial work on titanic model package

--- my-assignment-section-05/tests/conftest.py
+++ my-assignment-section-05/tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
-from regression_model.config.core import config
-from regression_model.processing.data_manager import load_dataset
+from classification_model.config.core import config
+from classification_model.processing.data_manager import load_dataset
 
 
 @pytest.fixture()
 def sample_input_data():
-    return load_dataset(file_name=config.app_config.test_data_file)
+    return load_dataset(file_name=config.app_config.raw_data_file)
